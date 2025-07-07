cb82fd0b5511bcc043ebc15f48c33cb82ff651b6
Jack <jwrauch02@gmail.com>
2025-07-07 15:44:05 -0400

    attempt for 80

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`freshzincs listening on port ${PORT}`));
