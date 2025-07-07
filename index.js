9939754efe66022ed685c45518592fbb48c99f7a
Jack <jwrauch02@gmail.com>
2025-07-07 12:35:22 -0400

    changing listening port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`freshzincs listening on port ${PORT}`));
